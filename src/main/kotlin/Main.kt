dc199e911a58cf48dfd87e1f53d1a385ddbf21ed
Melia Syhada <meliasyuhada5@gmail.com>
2022-03-11 11:29:11 +0700

    Initial project pertemuan1

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+//main merupakan proses awal atau eksekusi awal.
+//{} merupakan block kode awal dan akhir dalam bahasa kotlin.
+
+fun main() {
+    println("Hello, saya baru belajar kotlin!")
+}
+
+//kotlin lebih simpel dibandingkan bahasa lain/java
