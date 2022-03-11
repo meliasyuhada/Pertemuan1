1df3974f02ee2af568b3a19b459ec89b43b1fda6
Melia Syhada <meliasyuhada5@gmail.com>
2022-03-11 16:07:55 +0700

    Percobaan Operation

---
+++ src/main/kotlin/Operation.kt
@@ -0,0 +1,28 @@
+//komentar 1 baris
+/*
+komentar 1 baris atau lebih
+ */
+/**
+ * Dokumentasi
+ */
+
+fun main() {
+    var a = 7
+    var b = 6
+    var c = (a++ + b++) / 2
+    println(c + b)
+}
+
+// |^ hasilnya bilangan bulat
+
+
+/*
+fun main() {
+    var a = 7
+    var b = 6
+    var c = (a++ + b++) / 2.0
+    println(c + b)
+}
+*/
+
+//hasilnya berkoma 13,5
