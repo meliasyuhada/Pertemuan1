b543ece9a836524ad6a72894419387ecfce4c1f7
Melia Syhada <meliasyuhada5@gmail.com>
2022-03-11 20:17:02 +0700

    Percobaan Underscore Literal

---
+++ src/main/kotlin/UnderscoreLiteral.kt
@@ -0,0 +1,16 @@
+//pemberian undescord dalam kotlin bertujuan untuk memudahkan developer dalam membaca dan menulis kode.
+
+fun main(args: Array<String>) {
+    val oneMillion = 1_000_000_000
+    val creditCardNumber = 1234_5478_9087_7654L
+    val socialSecurityNumber = 999_99_9999L
+    val hexBytes = 0xFF_EC_DE_5E
+    val bytes = 0b11010010_01101001_10010010
+
+    println("oneMillion = $oneMillion")
+    println("creditCardNumber = $creditCardNumber")
+    println("socialSecurityNumber = $socialSecurityNumber")
+    println("hexBytes = $hexBytes")
+    println("bytes = $bytes")
+
+}
