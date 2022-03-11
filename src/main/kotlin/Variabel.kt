dcb6cfd8afabd24292dc4e691d5171e2e63cc0a9
Melia Syhada <meliasyuhada5@gmail.com>
2022-03-11 16:08:28 +0700

    Percobaan Variabel

---
+++ src/main/kotlin/Variabel.kt
@@ -0,0 +1,18 @@
+/**
+ * Variabel ada 2 yaitu var dan val
+ * var merupakan variabel yang nilai atau literal nya bisa dirubah ubah.
+ * sedangkan val merupakan variabel yang nilai atau literalnya tidak bisa diubah lagi.
+ * biasanya digunakan untuk konstanta.
+ *
+ */
+
+fun main() {
+    var angka = 5
+    angka = 10 //var bisa dirubah
+    println(angka)
+
+
+    val PHI = 3.14
+    // PHI = 22.9   => eror tidak bisa dirubah
+    println(PHI)
+}
