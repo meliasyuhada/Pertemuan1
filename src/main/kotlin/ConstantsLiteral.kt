edabc9ee6a47c09d0ba92cf50bedcfd8aa1c078e
Melia Syhada <meliasyuhada5@gmail.com>
2022-03-11 20:15:36 +0700

    Percobaan Constant Literal

---
+++ src/main/kotlin/ConstantsLiteral.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    val floatLiteral : Float = 178.95F //Literal Float
+    println("Contoh Literal Float : $floatLiteral")
+    val oneBillion : Long = 100000000L //Literal Long
+    println("Contoh Literal Long : $oneBillion")
+    val valHeksa = 0x0F //Literal heksadesimal diawali dengan 0x
+    println("Contoh Literal valHeksa : $valHeksa")
+    val valBinary = 0b00001011//Literal binary diawali dengan 0b
+    println("Contoh Literal valBinary : $valBinary")
+    val notasiKonversional = 123.5e10
+    println("Contoh Literal notasiKonversional : $notasiKonversional")
+
+}
