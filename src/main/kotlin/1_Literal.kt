a61ed121feb91dd145d1b324b8001e8abadbe665
Melia Syhada <meliasyuhada5@gmail.com>
2022-03-11 15:31:12 +0700

    Percobaan Literal

---
+++ src/main/kotlin/1_Literal.kt
@@ -0,0 +1,22 @@
+fun main() {
+    //deklarasi dan inisialisasi variabel-variabel
+    var intLiteral = 5
+    var doubleLiteral = .02
+    var stringLiteral = "Hello"
+    var charLiteral = '1'
+    var boolLiteral = true
+
+    println("intLiteral = $intLiteral")
+    println("doubleLiteral = $doubleLiteral")
+    println("stringLiteral = $stringLiteral")
+    println("charLiteral = $charLiteral")
+    println("boolLiteral = $boolLiteral")
+
+//    println("intLiteral = " + intLiteral)
+//    println("doubleLiteral = " + doubleLiteral)
+//    println("stringLiteral = " + stringLiteral)
+//    println("charLiteral = " + charLiteral)
+//    println("boolLiteral = " + boolLiteral)
+
+
+}
